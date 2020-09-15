c9d4cc33fac44d01604336a4195a384b7e910dad
Vivek Bhookya <v.bhookya98@gmail.com>
2020-09-14 23:36:19 -0500

    listBuilder.py, can construct the filepaths for our URL files

---
+++ listBuilder.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+def main():
+    # UNTIL MACHINE LEARNING MODEL,
+    #  THIS WILL HELP RENEW OUR LISTS
+    # Open URL dir
+    # dict for URL
+    # string for resulting list
+    # For file in 0..Z.txt
+    # Add url to dict, build string, concat to string
+    # write to file
+
+    # Do same thing with banned keywords
+    print("Hello!")
+
+    pathKeywords = "lists/Keywords/"
+    pathUrls = "lists/Urls/"
+
+    # print(urls.read())
+
+    ctr = 0
+    for c in "0123456789abcdefghijklmmopqrstuvwxyz":
+        # urls = open(pathUrls + c + ".txt", "r")
+        name = pathUrls + c + ".txt"
+        print(name)
+
+
+
+
+
+    # for url in urls:
+    #     ctr += 1
+    #
+    # print(ctr)
+
+
+
+
+if (__name__ == "__main__"):
+    main()

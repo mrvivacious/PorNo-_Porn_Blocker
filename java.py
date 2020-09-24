621522efac843647d46bd4e6a5f8c1e04e3d9248
Vivek Bhookya <v.bhookya98@gmail.com>
2020-09-24 12:08:33 -0500

    [21] java.py creates URL dict in Domains.java

--- java.py
+++ java.py
@@ -5,14 +5,14 @@ def main():
     # UNTIL MACHINE LEARNING MODEL,
     #  THIS WILL HELP RENEW OUR LISTS
 
-    DEV = "testFile.txt"
-    # PROD = "lists.java"
+    #DEV = "testFile.txt"
+    PROD = "Domains.java"
 
     PATH_URLS = "lists/Urls/"
     PATH_KEYWORDS = "lists/Keywords/"
 
     # w = [over]write, a = append
-    outputFile = open(DEV, "w")
+    outputFile = open(PROD, "w")
 
     header = "// PorNo!\n"
     header += "// lists.java THIS FILE WAS GENERATED WITH java.py\n"
@@ -35,22 +35,71 @@ def main():
     \tstatic String TAG = "!! Domains";
 
     \tpublic static Map<String, Boolean> init() {
-    \t\t// Add lists here, addA() addB() addC() etc
+    \t\tadd0();
+    \t\tadd1();
+    \t\tadd2();
+    \t\tadd3();
+    \t\tadd4();
+    \t\tadd5();
+    \t\tadd6();
+    \t\tadd7();
+    \t\tadd8();
+    \t\tadd9();
+    \t\taddA();
+    \t\taddB();
+    \t\taddC();
+    \t\taddD();
+    \t\taddE();
+    \t\taddF();
+    \t\taddG();
+    \t\taddH();
+    \t\taddI();
+    \t\taddJ();
+    \t\taddK();
+    \t\taddL();
+    \t\taddM();
+    \t\taddN();
+    \t\taddO();
+    \t\taddP();
+    \t\taddQ();
+    \t\taddR();
+    \t\taddS();
+    \t\taddT();
+    \t\taddU();
+    \t\taddV();
+    \t\taddW();
+    \t\taddX();
+    \t\taddY();
+    \t\taddZ();
+
+    \t\treturn dict;
+    \t}
+
+
     """
 
     outputFile.write(header)
 
-    s = "let pornMap={"
+    s = "\tstatic void add"
     for c in "0123456789abcdefghijklmnopqrstuvwxyz":
+        s += c.upper() + "() {\n\t\tString[] domains = {"
         name = PATH_URLS + c + ".txt"
         urls = open(name, "r")
 
         for url in urls:
-            urlWithoutNewLine = "\"" + url[:-1] + "\":!0"
+            urlWithoutNewLine = "\"" + url[:-1] + "\""
             s += urlWithoutNewLine + ","
 
-    s = s[:-1]
-    s += "};\n"
+        s = s[:-1]
+        s += "};\n\n"
+        s += "\t\tfor (String domain:domains) { dict.put(domain, true); }\n"
+
+        s += "\t}\n\n"
+
+        outputFile.write(s)
+        s = "\tstatic void add"
+
+    outputFile.write("}")
 
     #outputFile.write(s)
 

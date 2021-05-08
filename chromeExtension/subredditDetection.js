3bd4e8931ecd299059ff1f3c61d59f8b8f43be59
Vivek Bhookya <v.bhookya98@gmail.com>
2021-05-08 17:24:12 -0500

    [ChrEx] subreddit detection ready to ship (manually tested)

--- chromeExtension/subredditDetection.js
+++ chromeExtension/subredditDetection.js
@@ -3,17 +3,29 @@
 // NSFW subreddit detection (via the 'nsfw' post tags)
 // + We can add other post-load stuff in here too ie. keyword check, ibm api, etc
 
+function saveSubredditToBannedLinksList(domain) {
+  let splitPathname = window.location.pathname.split('/');
+  let URLFormattedWithSubreddit = domain + '/r/' + splitPathname[2];
+  URLFormattedWithSubreddit = URLFormattedWithSubreddit.toLowerCase();
+
+  // URL should look like "reddit.com/r/cscareerquestions[/]?"
+  // alert(URLFormattedWithSubreddit);
+
+  // Add this website to our database for faster subsequent redirections
+  // User can enter either of these formats so save both for now
+  store(URLFormattedWithSubreddit);
+  store(URLFormattedWithSubreddit + '/');
+}
+
 window.onload = () => {
   let host = window.location.hostname; // e621.net smh
   let domain = host.split('www.')[1];
 
   if (domain === 'reddit.com') {
     if (document.getElementsByClassName('_1wzhGvvafQFOWAyA157okr').length) {
-      document.firstElementChild.remove();
-
-      // todo add this website to our database for faster future-redirection
-
-      PorNo(); // window.stop() wont work as the page has finished downloading LOL
+      document.firstElementChild.remove(); // window.stop() wont work (.onload LOL)
+      saveSubredditToBannedLinksList(domain);
+      PorNo();
     }
   }
 }

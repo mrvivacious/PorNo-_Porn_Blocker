99874261099faa5b195ac300b12af1b2914827cf
Vivek Bhookya <v.bhookya98@gmail.com>
2021-07-15 03:38:26 -0500

    [Refactor ChrEx] Renamed NSFW Subreddit file + new function

--- chromeExtension/NSFWSubredditDetection.js
+++ chromeExtension/NSFWSubredditDetection.js
@@ -3,16 +3,19 @@
 // NSFW subreddit detection (via the 'nsfw' post tags)
 // + We can add other post-load stuff in here too ie. keyword check, ibm api, etc
 
+function subredditHasNSFWTag() {
+  return document.getElementsByClassName("_1x9diBHPBP-hL1JiwUwJ5J").length;
+}
+
 function getSubredditFromDomain(domain) {
-  let splitPathname = window.location.pathname.split('/');
-  let URLFormattedWithSubreddit = domain + '/r/' + splitPathname[2];
-  
+  let splitPathname = window.location.pathname.split("/");
+  let URLFormattedWithSubreddit = domain + "/r/" + splitPathname[2];
   return URLFormattedWithSubreddit.toLowerCase();
 }
 
 function saveSubredditToBannedLinksList(subredditURL) {
   // Today I learned there is r/undefined
-  if (subredditURL === 'reddit.com/r/undefined') {
+  if (subredditURL === "reddit.com/r/undefined") {
     return;
   }
 
@@ -21,20 +24,19 @@ function saveSubredditToBannedLinksList(subredditURL) {
   // Add this website to our database for faster subsequent redirections
   // User can enter either of these formats so save both for now
   store(subredditURL);
-  store(subredditURL + '/');
+  store(subredditURL + "/");
 }
 
 window.onload = () => {
   let host = window.location.hostname; // e621.net smh
-  let domain = host.split('www.')[1];
+  let domain = host.split("www.")[1];
 
-  if (domain === 'reddit.com') {
-    if (document.getElementsByClassName('_1x9diBHPBP-hL1JiwUwJ5J').length) {
+  if (domain === "reddit.com") {
+    if (subredditHasNSFWTag()) {
       document.firstElementChild.remove(); // window.stop() wont work (.onload LOL)
-      
       let subreddit = getSubredditFromDomain(domain);
       saveSubredditToBannedLinksList(subreddit);
       PorNo();
     }
   }
-}
+};
